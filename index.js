4c59809e557d61dda9dc34c7bcb17473bf309280
Simon3222 <simonmwangi3222@gmail.com>
2023-10-26 12:23:54 +0300

    to make it send image when one type /hi

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const token = '5809369871:AAGIgLfCilhkB2Te_FALcqjpG30nR3RPaFc';
 // Replace with your bot's token
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/hi/, (msg) => {
+bot.onText(/\/hi/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'hi 2');
+  const photoPath = 'https://telegra.ph/t1-10-26-2'; 
+// Replace with the actual path of your image
+
+  bot.sendPhoto(chatId, photoPath, {
+    caption: 'Hello! This is your image.'
+  });
 });
